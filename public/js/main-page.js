d030e69c105407d4bffa936ef45ea941ccc38d0c
Oleg Evseev <ixen.eoa@me.com>
2015-03-17 23:06:45 +0300

    Index page finalized

--- public/js/main-page.js
+++ public/js/main-page.js
@@ -3,42 +3,63 @@
   var socket;
 
   /**
+   * Центрирование модального окна
+   */
+  function centerModal() {
+    var $modal = $(this);
+    var $dialog = $modal.find('.modal-dialog');
+
+    $modal.css('display', 'block');
+    $dialog.css("margin-top", Math.max(0, ($(window).height() -
+      $dialog.height()) / 2));
+  }
+
+  /**
    * Показ оверлея с сообщением о загрузке
    */
-  var displayLoading = function (message) {
-  };
+  function displayLoading(message) {
+    $('#loading-window.message').text(message);
+    $('#loading-window').modal('show'); 
+  }
 
   /**
    * Подключение к сокету и отправка сообщения
    */
-  var emit = function (eventName) {
+  function emit(eventName) {
     socket = io.connect();
     socket.once('roomIDReturned', function onRoomFound(roomID) {
       window.location.replace('/id/' + roomID);
     });
     socket.emit(eventName);
-  };
+  }
 
   /**
-   * Установка обработчиков нажатий кнопок
+   * Инициализация
    */
-  var setButtonActions = function () {
+  function init() {
+    // Инициализация модального окна загрузки
+    $('#loading-window').on('show.bs.modal', centerModal);
+    $(window).on('resize', function() {
+      $('#loading-window:visible').each(centerModal);
+    });
+
+    // Запрет на закрытие модального окна
+    $('#loading-window').modal({
+      backdrop: 'static',
+      keyboard: false,
+      show: false
+    });
+
+    // Установка обработчиков событий кнопок
     $('#find-game').click(function () {
       displayLoading("Идет поиск игры.");
       emit('findRoom');
     });
     $('#new-game').click(function () {
-      displayLoading("Идет создание комнаты.")
+      displayLoading("Идет создание комнаты.");
       emit('newRoom');
     });
-  };
-
-  /**
-   * Инициализация
-   */
-  var init = function () {
-    setButtonActions();
-  };
+  }
 
   $(init);
 })(jQuery);
